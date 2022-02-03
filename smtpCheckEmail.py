cb025b92ebfad30b34f68aee61e5867049d673ba
atomdog <agom@bu.edu>
2022-02-03 13:15:06 -0500

    Basic email checking script

---
+++ smtpCheckEmail.py
@@ -0,0 +1,18 @@
+def checkemail():
+    inbox = []
+    with MailBox('imap.gmail.com').login('email', 'password', 'INBOX') as mailbox:
+    # get unseen emails from INBOX folder
+        for msg in mailbox.fetch(AND(seen=False)):
+            mtext = msg.html
+            b = mtext.find("<td>")
+            e = mtext.find("</td>")
+            mtext = mtext[b+4:e]
+            cutter = False
+            counter = -1
+            unpad = len(mtext)-48
+            mtext = mtext.replace("\n", "")
+            mtext = mtext.replace("\r", "")
+            mtext = mtext.lstrip()
+            mtext = mtext.rstrip()
+            inbox.append([msg.from_values["email"], mtext])
+            return(inbox)

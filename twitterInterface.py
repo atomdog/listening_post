7a3eeb3d67f7ee05c7471afdbaf212c59d5a6885
atomdog <agom@bu.edu>
2022-03-17 17:45:36 -0400

    polishing more

--- twitterInterface.py
+++ twitterInterface.py
@@ -9,6 +9,7 @@ def replybyid(id, user, text, tuser):
     api.update_status('@'+str(user)+' ' + str(text),tweetId)
 #=============================================
 
+#===========in progress functions===========
 def tweets_since_x_by_user(screen_name, api, since):
     alltweets = []
     new_tweets = api.user_timeline(screen_name = screen_name,count=200)
@@ -23,46 +24,7 @@ def tweets_since_x_by_user(screen_name, api, since):
         print(f"...{len(alltweets)} tweets downloaded so far")
     outtweets = [[tweet.id_str, tweet.created_at, tweet.text] for tweet in alltweets]
     return(outtweets)
-
-
-
-def tweetsbyuser(screen_name, api):
-    alltweets = []
-    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
-    alltweets.extend(new_tweets)
-    oldest = alltweets[-1].id - 1
-    while len(new_tweets) > 0:
-        print(f"getting tweets before {oldest}")
-        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
-        alltweets.extend(new_tweets)
-        oldest = alltweets[-1].id - 1
-        print(f"...{len(alltweets)} tweets downloaded so far")
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.text] for tweet in alltweets]
-    return(outtweets)
-
-
-def repliesbyid(id, api):
-    replies=[]
-    for tweet in tweepy.Cursor(api.search,q='to:'+name, result_type='recent', timeout=999999).items(1000):
-        if hasattr(tweet, 'in_reply_to_status_id_str'):
-            if (tweet.in_reply_to_status_id_str==tweet_id):
-                replies.append(tweet)
-    with open('raw_twit/replies|' + str(tweet_id) + '|.csv', 'a') as f:
-        csv_writer = csv.DictWriter(f, fieldnames=('user', 'text'))
-        csv_writer.writeheader()
-        for tweet in replies:
-            row = {'user': tweet.user.screen_name, 'text': tweet.text.replace('\n', ' ')}
-            csv_writer.writerow(row)
-
-def followersbyuser(user, api):
-    screen_name = user
-    c = tweepy.Cursor(api.followers, screen_name)
-    with open('raw_twit/followers|' + str(user) + '|.csv', 'w') as f:
-        csv_writer = csv.DictWriter(f, fieldnames=('user',))
-        csv_writer.writeheader()
-        for follower in c.items():
-            row = {'user': follower.screen_name}
-            csv_writer.writerow(row)
+#==========================================
 
 #===========polished functions=============
 
@@ -100,4 +62,16 @@ def usertoid(username, api):
     userid = api.get_user(username)
     ID = userid.id_str
     return(ID)
+
+def repliestotweet(id, api):
+    replies=[]
+    returnreplies = []
+    for tweet in tweepy.Cursor(api.search,q='to:'+name, result_type='recent', timeout=999999).items(1000):
+        if hasattr(tweet, 'in_reply_to_status_id_str'):
+            if (tweet.in_reply_to_status_id_str==tweet_id):
+                replies.append(tweet)
+        for tweet in replies:
+            returnreplies.append([tweet.created_at,tweet.user.screen_name, tweet.text.replace('\n', ' ')])
+    return(returnreplies)
+
 #==========================================
